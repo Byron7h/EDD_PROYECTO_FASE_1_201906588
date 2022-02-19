94f1c508e3e2dcf4cecd02f7a39f423e7f20800c
byron <solisbyron711@gmail.com>
2022-02-19 04:13:45 -0600

    Agregamos el objeto ventanilla

---
+++ src/proyecto_1_estructuras/ventanilla.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_1_estructuras;
+
+/**
+ *
+ * @author usuario
+ */
+public class ventanilla {
+    // Atributos
+    int id;
+    boolean disponible = true; // a la hora de su creación todas están disponibles
+    int img_c = 0;
+    int img_bn = 0;
+    
+    public ventanilla(int num){
+        this.id = num;
+    }
+    
+    public boolean recibir_img (nodo evaluado){ // debolberá true cuando deje la ventanilla vacia, es la señal para pasar al siguiente nodo
+        if (disponible){
+            disponible = false;
+        }
+        int nc = evaluado.get_img_c();
+        int nbn = evaluado.get_img_c();
+
+        
+        // Valuando primero las de color
+        if ( img_c != nc){
+            img_c ++;
+            System.out.println("se agregó una a color a la ventanilla" + id);               
+        }else if(img_bn != nbn){
+            img_bn ++;
+            System.out.println("se agregó una a blanco y negro a la ventanilla" + id); 
+        }else{
+            System.out.println("El cliente " + evaluado.get_nombre()+ "avanza a la listra de espera");
+            disponible = true;
+                    
+        }
+        return disponible;
+        
+       
+        // desde acá creo que vamos a imprimir, antes de colocarle que la caja ahora está disponible
+        
+    }
+    
+    
+    // hasta que la respuesta sea un true va a seguir itrando 
+    // manda como parámetro un nodo
+    
+    
+    
+    
+    
+}
